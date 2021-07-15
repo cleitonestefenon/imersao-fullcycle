d1ac92b9cd6180f0e656f805e7ea63bfb79492e8
Cleiton Estefenon <cleitonestefenon@gmail.com>
2021-07-15 14:41:50 -0300

    Criado toda estrutura para acomodar as configurações do gRPC que será responsável por converter as informações que irão trafegar na rede em binario, deixando as transferências mas leves e menos custosas para nossos serviços Client e Consumer

---
+++ backend-bank-golang/infrastructure/grpc/server/server.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"log"
+	"net"
+
+	"github.com/cleitonestefenon/imersao-fullcycle/infrastructure/grpc/pb"
+	"github.com/cleitonestefenon/imersao-fullcycle/infrastructure/grpc/service"
+	"github.com/cleitonestefenon/imersao-fullcycle/usecase"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type GRPCServer struct {
+	ProcessTransactionUseCase usecase.UseCaseTransaction
+}
+
+func NewGRPCServer() GRPCServer {
+	return GRPCServer{}
+}
+
+func (g GRPCServer) Serve() {
+	lis, err := net.Listen("tcp", "0.0.0.0:50052")
+	if err != nil {
+		log.Fatalf("could not listen tpc port")
+	}
+	transactionService := service.NewTransactionService()
+	transactionService.ProcessTransactionUseCase = g.ProcessTransactionUseCase
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+	pb.RegisterPaymentServiceServer(grpcServer, transactionService)
+	grpcServer.Serve(lis)
+}
